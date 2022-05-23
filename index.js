aa68142a72349a7dbb748e9e4ce06e62f29a9c24
SpeedBird789 <asifmahmoud787@gmail.com>
2022-05-23 21:49:12 +0100

    server connected and tools shown on UI

--- index.js
+++ index.js
@@ -1,13 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion } = require('mongodb');
+
 
 const app = express();
+const port = process.env.PORT || 5000;
 
 // Using middleware
 app.use(cors());
 app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ielft.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run(){
+    try{
+        await client.connect();
+        const toolCollection = client.db('manufacturing').collection('tools');
+
+        app.get('/tool', async(req, res)=>{
+            const query = {};
+            const cursor = toolCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
+
+
 
 app.get('/', (req, res) => {
     res.send('Running manufacturing server portal');
