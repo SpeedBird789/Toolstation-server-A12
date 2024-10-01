09fe3566bc8a76f257e249629c3887e482b5c26d
Asif Mahmoud <asifmahmoud787@gmail.com>
2024-10-01 17:03:09 +0100

    latest server repo

--- index.js
+++ index.js
@@ -10,12 +10,20 @@ const port = process.env.PORT || 5000;
 
 // Using middleware
 app.use(cors());
+
+  
 app.use(express.json());
 
+// Original 2022
+/* const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ielft.mongodb.net/?retryWrites=true&w=majority`;
+*/
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ielft.mongodb.net/?retryWrites=true&w=majority`;
+
+// 2024
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster2024.cfbcm.mongodb.net/?retryWrites=true&w=majority&appName=Cluster2024`;
 
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
 
 function verifyJWT(req, res, next) {
     const authHeader = req.headers.authorization;
@@ -163,7 +171,7 @@ app.get('/', (req, res) => {
 
 
 app.listen(port, ()=>{
-    console.log('Manufactring server is running');
+    console.log(`Manufacturing server is running on port: ${port}`);
 })
 
 // Whoash!
