271abc4e250e33cfbdbfd6172eeb348b1316d2f0
Arham <arhamawan99@gmail.com>
2024-10-03 03:57:31 +0500

    🐛 FIX: cors issue fix

--- api/index.js
+++ api/index.js
@@ -1,24 +1,12 @@
-const createError = require('http-errors');
-const express = require('express');
-const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
+var express = require('express');
 
-// Import routes
-const indexRouter = require('../routes');
+var indexRouter = require('../routes');
 
-// Initialize express app
-const app = express();
+var app = express();
 
-// Set logger, cookieParser, and express.json
-app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
-// CORS Whitelist Setup
-const whitelist = ['*'];  // Adjust the allowed origins in the whitelist array
+const whitelist = [
+  '*'
+];
 
 app.use((req, res, next) => {
   const origin = req.get('referer');
@@ -34,25 +22,12 @@ app.use((req, res, next) => {
   else next();
 });
 
-// Set request context middleware
 const setContext = (req, res, next) => {
   if (!req.context) req.context = {};
   next();
 };
 app.use(setContext);
 
-// Use routers for different routes
 app.use('/', indexRouter);
-
-// Catch 404 and forward to error handler
-app.use((req, res, next) => {
-  next(createError(404));
-});
-
-// Error handler
-app.use((err, req, res, next) => {
-  res.status(err.status || 500);
-  res.json({ error: err.message });
-});
 
 module.exports = app;
--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const jwt = require('jsonwebtoken');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 // Initialize Router
