95ba23a45a20bb6f12a2cd267bfa61aea860ac14
Arham <arhamawan99@gmail.com>
2024-10-03 04:30:44 +0500

    👍 IMPROVE: fixess

--- api/index.js
+++ api/index.js
@@ -6,7 +6,7 @@ const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 
 
 const app = express();
-const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5001;
 
 const options = [
     cors({
